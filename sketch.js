9fbb13eb9a3852110e5719b3d8e60f1bf613cb12
laurenstuerk <laurenstuerk@gmail.com>
2024-08-27 23:00:25 +0200

    File fix again
    :(

--- sketch.js
+++ sketch.js
@@ -1,32 +1,32 @@
-let buttonX = 200;
-let buttonY = 200;
+let buttonX = 0;
+let buttonY = 0;
 let distanceToButton = 0.0;
 let backColor = "#000000";
 
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(displayWidth, displayHeight);
   noStroke();
   rectMode(CENTER);
+  buttonX = width/2
+  buttonY = height/2
 }
 
 function draw() {
+  
   background(backColor);
   distanceToButton = dist(buttonX, buttonY, mouseX, mouseY);
-  
-  rect(buttonX*1, buttonY*1, 50, 50, 15);
-  
-  
-  
-  
+  fill("#FFFFFF")
+  rect(buttonX, buttonY, 50, 50, 15);
 }
 
 function mousePressed() {
   if (mouseIsPressed && distanceToButton <= 25) {
-    buttonX = random(30, 370);
-    buttonY = random(30, 370);
+    buttonX = random(width*0.1, width*0.9);
+    buttonY = random(height*0.1, height*0.9);
     backColor = "#000000";
   }
   if (distanceToButton > 25) {
     backColor = "#FF0000";
   }
+  
 }
